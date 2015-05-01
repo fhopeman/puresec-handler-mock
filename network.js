e998653a6c1d390b48f18fdfc0815b2247d77f21
Fabian Hoffmann <fabian.hoffmann@otto.de>
2015-05-01 15:45:18 +0200

    renaming

--- network.js
+++ network.js
@@ -3,10 +3,10 @@ var os = require( 'os' );
 var currentCallbackAddress = function() {
     var networkInterfaces = os.networkInterfaces( );
     for (var networkInterface in networkInterfaces) {
-        var interface = networkInterfaces[networkInterface];
+        var iface = networkInterfaces[networkInterface];
 
-        for (var i = 0; i < interface.length; i++) {
-            var name = interface[i];
+        for (var i = 0; i < iface.length; i++) {
+            var name = iface[i];
             if (name.address !== '127.0.0.1' && name.family === 'IPv4' && !name.internal) {
                 return "http://" + name.address;
             }
