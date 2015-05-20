836b03b4acfab6e84253c6be4c5ed793ae996877
Fabian Hoffmann <fabianhoffmann_@web.de>
2015-05-20 20:01:39 +0200

    upgrade microservice-js verison to 0.3.0

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ var bodyParser = require('body-parser');
 var logger = require('winston');
 var puresecMicroservice = require("puresec-microservice-js");
 
-var app = express();
-var pmsUtils = puresecMicroservice.utils();
-
 var urlMaster = process.env.MASTER_URL || process.argv[2] || "http://localhost:3000";
 var registrationInterval = process.env.MASTER_REGISTRATION_INTERVAL || process.argv[3] || 5;
 var port = process.env.PORT || process.argv[4] || 3002;
+
+var app = express();
+var master = puresecMicroservice.master(urlMaster);
+var utils = puresecMicroservice.utils();
+var webApp = puresecMicroservice.webApp();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-pmsUtils.addHealthCheck(app, function() {
+webApp.registerHealthCheckEndpoint(app, function() {
     logger.info("health: UP");
 });
 
-pmsUtils.addNotifyEndpoint(app, function() {
+webApp.registerNotificationEndpoint(app, function() {
     logger.info("\nnotification received ..");
     logger.info(req.body);
 });
 
 app.listen(port, function() {
-    var urlClient = pmsUtils.currentAddress() + ":" + port;
-    var master = puresecMicroservice.master(urlMaster);
-
+    var urlClient = utils.currentAddress() + ":" + port;
     var registerOptions = {
         name: "Mock Handler 1",
         description: "Mock implementation of handler",
@@ -37,7 +37,7 @@ app.listen(port, function() {
             logger.info("registration result: ", jsonBody);
         },
         onError: function(error) {
-            logger.info("error during registration", error, "\nretry ..");
+            logger.error("error during registration", error, "\nretry ..");
             setTimeout(function() {
                 master.register(registerOptions);
             }, registrationInterval * 1000);
